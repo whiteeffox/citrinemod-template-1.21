975df5bc5b24ef4debf3760ef3352f31a81d7c28
fox <2511709688@qq.com>
2024-09-12 00:03:27 +0800

    Initial commit

---
+++ src/main/java/com/whiteeffox/citrine/CitrineModClient.java
@@ -0,0 +1,10 @@
+package com.whiteeffox.citrine;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CitrineModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
